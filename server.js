965eed31c6f767823a0c3823bc8a3a9504ab03db
Av3rageKing <your-email@example.com>
2026-05-04 18:50:01 +0100

    update

--- server.js
+++ server.js
@@ -1,35 +1,15 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
 const path = require("path");
-
-const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/users");
-const friendRoutes = require("./routes/friends");
-const avatarRoutes = require("./routes/avatar");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/cubera";
-
-mongoose.connect(MONGO_URI)
-  .then(() => console.log("Connected"))
-  .catch(err => console.log(err));
-
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/friends", friendRoutes);
-app.use("/api/avatar", avatarRoutes);
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log("Running on port " + PORT);
+  console.log("Server running on port " + PORT);
 });
