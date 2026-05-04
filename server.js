faf45c860df16395640aa1096f7f746b5f218241
Av3rageKing <your-email@example.com>
2026-05-04 18:31:45 +0100

    update

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Serve static files (like index.html)
 app.use(express.static(path.join(__dirname)));
 
 app.listen(PORT, () => {
-    console.log(`Mini Roblox server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
